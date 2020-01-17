49739b398019a9453904616434d90c06be9ca988
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-17 16:34:50 +0100

    add redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
 
 function App() {
   return (
+
     <div className="App">
       
     </div>
