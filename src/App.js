d9646f4db594beb60de515279d30af2f3ae68af5
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-18 12:55:10 +0100

    built basic layouts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 import reducers from "./reducers";
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import Main from "./layouts/Main";
 
 const store = createStore(reducers);
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
-      
+        <Header title = {"CPU Allocation"}/>
+        <Main/>
+        <Footer/>
       </div>
     </Provider>
   );
