2c8955a4e0b1000d2e506ddbf3d3cd10281d3bef
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 21:05:00 +0100

    change algorothmList component

--- src/components/AlgorithmList.js
+++ src/components/AlgorithmList.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {connect} from "react-redux";
+import {changeAlgorithm} from "../actions/index";
 import {algorithms} from "../settings.json";
 import Button from './Button';
-const AlgoritmList = () => {
+const AlgoritmList = ({algorithmText,changeAlgorithm,showList}) => {
     return ( 
-        <ul className = "algorithmList">
-            {algorithms.map((algorithm =><li className="algorithmList__item"> <Button text={algorithm} className="algorithmList__button" /> </li>))}
-        </ul>
+        <div>
+            {showList?
+            <ul className = "algorithmList">
+                {algorithms.map((algorithm =><li key={algorithm} className="algorithmList__item"> <Button  text={algorithm} onClick={()=>{changeAlgorithm(algorithm)}}className="algorithmList__button" /> </li>))}
+            </ul>:null}
+            <p>{algorithmText!==""?algorithmText:"proszę wybrać algorytm"}</p>
+        </div>
     );
 }
  
-export default AlgoritmList;
+const mapStateToProps = state =>{
+    return{
+        algorithmText: state.allocation.algorithm,
+        showList: state.componentState.algorithmListShow
+    }
+}
+
+export default connect(mapStateToProps,{changeAlgorithm})(AlgoritmList);
