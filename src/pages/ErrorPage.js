438cf63f3985880a15b1bba629d367faf28fbca8
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 21:01:31 +0100

    rename and add body

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ErrorPage = () => {
-    return "Error";
+    return (
+        <div className="error">
+            <h2 className="error__text">Przepraszamy strona nie znalezniona</h2>
+        </div>
+    );
 }
  
 export default ErrorPage;
