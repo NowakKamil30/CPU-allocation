19b4963390859f9653259e26fdc51dc1fb3221bd
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 12:52:53 +0100

    add className to header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = ({title}) =>{
     return(
-        <header>
-            <h1>
+        <header className="header">
+            <h1 className="header__title">
                 {title}
             </h1>
         </header>
