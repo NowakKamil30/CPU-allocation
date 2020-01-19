53ef0595b9226142030cfdfa59263524b8b7c801
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 18:10:43 +0100

    add button.js

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({onClick, text, className}) => {
+    return (
+        <button onClick={onClick} className={className}>
+            {text}
+        </button>
+    )
+}
+ 
+export default Button;
