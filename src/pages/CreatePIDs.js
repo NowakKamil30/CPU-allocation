f2183d3e06d561c50df0cccb30e9ab509c240996
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 18:08:59 +0100

    build basic components

---
+++ src/pages/CreatePIDs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreatePIDs = () =>{
+    return "PID";
+}
+
+export default CreatePIDs;
---
+++ src/pages/ShowStatistics.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShowStatistics = () => {
+    return "ShowStatistics";
+}
+ 
+export default ShowStatistics;
