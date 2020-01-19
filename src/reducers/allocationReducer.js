837c0c6f7fd375de140058b6d9fcba6aa5be56e4
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 21:03:06 +0100

    add new action to allocationReducer.js

--- src/reducers/allocationReducer.js
+++ src/reducers/allocationReducer.js
@@ -1,12 +1,17 @@
-const INITIAL_STATE = {pids:[]};
+import {change_algorithm} from "../actions/types";
+
+
+const INITIAL_STATE = {pids:[], algorithm:""};
 
 export default (state = INITIAL_STATE, action) => {
   console.log(action);
   switch (action.type) {
-
-
+    case change_algorithm:{
+      const {payload} = action;
+      return {...state,algorithm:payload};
+    }
     default:{
       return state;
-    }break;
+    }
   }
 };
