c626fabd4662d93c8f552a23180a4cbaa580278c
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-19 18:09:54 +0100

    use Route

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Route,Switch}  from "react-router-dom";
+import Home from "../pages/Home";
+import CreatePIDs from "../pages/CreatePIDs";
+import ShowChart from "../pages/ShowChart";
+import ShowSimulation from "../pages/ShowSimulation";
+import ShowStatistics from "../pages/ShowStatistics";
+import ErrorPage from "../pages/ErrorPage";
 
 const Main = () =>{
     return (
         <main>
-            
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/createPIDs" exact component={CreatePIDs}/>
+                <Route path="/showSimulation" exact component={ShowSimulation}/>
+                <Route path="/showChart" exact component={ShowChart}/>
+                <Route path="/showStatistics" exact component={ShowStatistics}/>
+                <Route component={ErrorPage}/>
+            </Switch>
         </main>
     )
 }
