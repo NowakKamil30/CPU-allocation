1340b6579b2001e303a5b1eca41e104df2c96b9a
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-20 01:36:06 +0100

    edit Aside.js

--- src/layouts/Aside.js
+++ src/layouts/Aside.js
@@ -1,7 +1,24 @@
 import React from 'react';
-
-const Aside = ()=>{
-    return "aside";
+import {connect} from "react-redux";
+import PID from "../components/PID";
+const Aside = ({pids})=>{
+    return (
+        <aside>
+            {pids.map(({name,tasks,priority,id})=>(
+                <PID
+                key={id}
+                name={name}
+                tasks={tasks}
+                priority={priority}
+                id={id}
+                />
+            ))}
+        </aside>
+    );
 }
-
-export default Aside;
+const mapStateToProps = state =>{
+    return{
+        pids:state.allocation.pids
+    }
+}
+export default connect(mapStateToProps,{})(Aside);
