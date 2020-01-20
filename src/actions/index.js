2014550a151a7a81a93e36607bb20abf50def26c
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-20 01:35:32 +0100

    add new actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
-import {change_algorithm,show_algorithm_list} from "./types";
+import {change_algorithm,
+    show_algorithm_list,
+    add_task_to_pid,
+    edit_task_pid,
+    name_pid,
+    priority_pid,
+    reset_pid,add_pid} from "./types";
 
 export const changeAlgorithm = (algoritm) =>(
     {
@@ -9,5 +15,40 @@ export const changeAlgorithm = (algoritm) =>(
 export const showAlgorithmList = () =>(
     {
         type:show_algorithm_list,
+    }
+)
+export const editTaskPid = (index,value)=>(
+    {
+        type:edit_task_pid,
+        payload:{index,value}
+    }
+)
+export const addTaskToPid = () =>(
+    {
+        type:add_task_to_pid,
+        payload:"CPU"
+    }
+)
+export const namePid = name =>(
+    {
+        type:name_pid,
+        payload:name
+    }
+)
+export const priorityPid = priority =>(
+    {
+        type: priority_pid,
+        payload: priority
+    }
+)
+export const resetPid = ()=>(
+    {
+        type:reset_pid,
+    }
+)
+export const addPid = (pid) =>(
+    {
+        type:add_pid,
+        payload:pid
     }
 )
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,8 @@
 export const change_algorithm = "CHANGE_ALGORITHM";
 export const show_algorithm_list="SHOW_ALGORITHM_LIST";
+export const add_task_to_pid="ADD_TASK_TO_PID";
+export const edit_task_pid="EDIT_TASK_PID";
+export const name_pid="NAME_PID";
+export const priority_pid="PRIORITY_PID";
+export const reset_pid="RESET_PID";
+export const add_pid="ADD_PID";
