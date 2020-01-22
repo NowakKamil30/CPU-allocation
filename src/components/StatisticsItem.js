793803908b228544d0574f3d358a31eeaffb9e4d
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-22 02:50:15 +0100

    add new component

---
+++ src/components/StatisticsItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    cpuAll,
+    ioAll} from "../helpers/forStatistics";
+
+const StatisticsItem = ({name,id,waiting,tasks,dataPids}) => {
+    return ( 
+        <li className="statistics__item">
+        <p className="statistics__name">{name}({id})</p>
+        <p className="statistics__text">waiting: {waiting}</p>
+        <p className="statistics__text">response: {waiting}</p>
+        <p className="statistics__text">GPU: {cpuAll([{tasks}])/cpuAll(dataPids)*100}%</p>
+        <p className="statistics__text">IO: {ioAll([{tasks}])/ioAll(dataPids)*100}%</p>
+        <p className="statistics__text">Turnaround: {tasks.length+waiting}</p>
+    </li>
+     );
+}
+ 
+export default StatisticsItem;
