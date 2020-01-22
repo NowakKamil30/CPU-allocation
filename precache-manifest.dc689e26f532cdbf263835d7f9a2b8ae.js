9dd6e5fb6ea43cdca692ff068a0a17e3d19153f2
Kamil Nowak <kamilnowakx98@gmail.com>
2020-01-22 18:34:16 +0100

    Updates

---
+++ precache-manifest.dc689e26f532cdbf263835d7f9a2b8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdffdb587c0e8588b13d30d79d10bb1e",
+    "url": "/CPU-allocation/index.html"
+  },
+  {
+    "revision": "d71140a682034e013181",
+    "url": "/CPU-allocation/static/css/main.ad3ae3e4.chunk.css"
+  },
+  {
+    "revision": "d6b5c8adf8c8e37e5e17",
+    "url": "/CPU-allocation/static/js/2.8335c2b7.chunk.js"
+  },
+  {
+    "revision": "9b499988666cc8d7d52d70a8c3b2f162",
+    "url": "/CPU-allocation/static/js/2.8335c2b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d71140a682034e013181",
+    "url": "/CPU-allocation/static/js/main.584d6ee1.chunk.js"
+  },
+  {
+    "revision": "2961aecff32656e9b46c",
+    "url": "/CPU-allocation/static/js/runtime-main.192e1345.js"
+  }
+]);
